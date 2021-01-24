96c9cb7e2f01290e110e6fbb5a761727b8e32ea7
SomeonePL <skrobak.andrzej@gmail.com>
2021-01-24 15:11:52 +0100

    Broadcasting peers and basic view

---
+++ app/views.py
@@ -0,0 +1,49 @@
+import datetime
+import json
+
+import requests
+from flask import render_template, redirect, request
+
+from app import app
+
+# The node with which our application interacts, there can be multiple
+# such nodes as well.
+CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8001"
+
+posts = []
+
+
+def fetch_posts():
+    """
+    Function to fetch the chain from a blockchain node, parse the
+    data and store it locally.
+    """
+    get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        content = []
+        chain = json.loads(response.content)
+        print(chain)
+        for block in chain["chain"]:
+            for tx in block["transaction"]:
+                tx["index"] = block["index"]
+                tx["hash"] = block["previous_hash"]
+                content.append(tx)
+
+        global posts
+        posts = sorted(content, key=lambda k: k['timestamp'],
+                       reverse=True)
+
+
+@app.route('/')
+def index():
+    fetch_posts()
+    return render_template('index.html',
+                           title='Albicla: Let\'s All Be Clear',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)
+
+
+def timestamp_to_string(epoch_time):
+    return datetime.datetime.fromtimestamp(epoch_time).strftime('%H:%M')
